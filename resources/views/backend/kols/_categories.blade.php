aeb9a7c85495b9efc9dfed3d4841ec047ec081ab
cuongnv238 <nguyencuong23893@gmail.com>
2025-11-04 10:53:03 +0700

    update

--- resources/views/backend/kols/_categories.blade.php
+++ resources/views/backend/kols/_categories.blade.php
@@ -1,5 +1,6 @@
-<select name="language" data-placeholder="{{ __('Chọn ngôn ngữ') }}"
+<select name="language" data-placeholder="{{ __('Chọn danh mục') }}"
     class="form-control form-control-select2 @error('language')is-invalid @enderror">
+    <option></option>
     @foreach($categories as $category)
     <option {{ old('language') == $category->name ? 'selected' : null }} value="{{ $category->name }}">
         {{ $category->name }}</option>
